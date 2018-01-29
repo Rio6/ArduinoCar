58f921e98660e9c9ff1f8baa04ace1fbb2afafe6
Rio6 <rio6.liu@gmail.com>
2018-01-28 17:30:32 -0800

    Sends jpeg compressed image

--- car/src/net/rio/car/CameraController.java
+++ car/src/net/rio/car/CameraController.java
@@ -1,13 +1,14 @@
 package net.rio.car;
 
 import android.app.Activity;
-import android.graphics.ImageFormat;
+import android.graphics.*; // ImageFormat, YuvImage, Rect
 import android.hardware.Camera;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.*; // SurfaceView, SurfaceHolder
 
 import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 
 @SuppressWarnings("deprecation") // Camera is deprecated after API 23
 public class CameraController implements SurfaceHolder.Callback, Camera.PreviewCallback {
@@ -15,6 +16,9 @@ public class CameraController implements SurfaceHolder.Callback, Camera.PreviewC
     private Camera camera;
     private SurfaceView cameraView;
     private OnCameraDataListener cameraListener;
+
+    private ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    private Rect preRect;
 
     public CameraController(Activity activity, OnCameraDataListener cameraListener) {
         this.cameraListener = cameraListener;
@@ -60,6 +64,8 @@ public class CameraController implements SurfaceHolder.Callback, Camera.PreviewC
                 Camera.Parameters parameters = camera.getParameters();
                 Camera.Size size = camera.getParameters().getPreviewSize();
 
+                preRect = new Rect(0, 0, size.width, size.height);
+
                 camera.addCallbackBuffer(
                         new byte[size.width * size.height *
                         ImageFormat.getBitsPerPixel(parameters.getPreviewFormat()) / 8]);
@@ -75,7 +81,10 @@ public class CameraController implements SurfaceHolder.Callback, Camera.PreviewC
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-        cameraListener.onCameraData(data);
+        YuvImage frameImage = new YuvImage(data, ImageFormat.NV21, preRect.right, preRect.bottom, null);
+        frameImage.compressToJpeg(preRect, 50, byteStream);
+        cameraListener.onCameraData(byteStream.toByteArray());
+        byteStream.reset();
         camera.addCallbackBuffer(data);
     }
 
